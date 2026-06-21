36e3a3b5ab57331262012d83c4e88ee59aab39a5
Brinley Macnamara <brinleymacnamara@Brinleys-MacBook-Pro.local>
2026-06-21 01:13:04 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import socket
 
 
 def main():
-    OK_RESP = b"HTTP/1.1 200 OK\r\n\r\n"
+    OK_RESP = "HTTP/1.1 200 OK\r\n\r\n"
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
     server_socket.send(OK_RESP)
