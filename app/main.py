7a487eee0d1be1f6ce3edb1ee9e2a6bae355e3ff
Brinley Macnamara <brinleymacnamara@Brinleys-MacBook-Pro.local>
2026-06-21 04:37:47 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import threading
 import socket
 
 @dataclass
@@ -7,25 +8,6 @@ class HttpRequest:
     target: str
     version: str
     headers: dict[str, str]
-
-def parse_request(raw_bytes: bytes) -> HttpRequest:
-    request = raw_bytes.decode()
-    lines = request.splitlines()
-
-    # request line
-    request_line = lines[0]
-    method, target, version = request_line.split()
-
-    # headers
-    headers = {}
-    for line in lines[1:]:
-        if not line:
-            break
-        
-        header_type, value = line.split(":", maxsplit=1)
-        headers[header_type.strip().lower()] = value.strip()
-
-    return HttpRequest(method, target, version, headers)
 
 def route_request(request: HttpRequest) -> bytes:
     target = request.target
@@ -44,20 +26,47 @@ def route_request(request: HttpRequest) -> bytes:
     else:
         return b"HTTP/1.1 404 Not Found\r\n\r\n"
 
+def parse_request(raw_bytes: bytes) -> HttpRequest:
+    request = raw_bytes.decode()
+    lines = request.splitlines()
+
+    # request line
+    request_line = lines[0]
+    method, target, version = request_line.split()
+
+    # headers
+    headers = {}
+    for line in lines[1:]:
+        if not line:
+            break
+
+        header_type, value = line.split(":", maxsplit=1)
+        headers[header_type.strip().lower()] = value.strip()
+
+    return HttpRequest(method, target, version, headers)
+    
+def handle_conn(conn):
+    with conn:
+        while True: 
+            raw_bytes = conn.recv(1024) # Receive data from socket
+            
+            if not raw_bytes: # Detect closed connection
+                break
+           
+            request = parse_request(raw_bytes)
+            response = route_request(request)
+
+            conn.sendall(response)
+
 def main():
     # Setup connection
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, _ = server_socket.accept() # wait for client
 
-    # Receive + parse request
-    raw_bytes = conn.recv(1024)
-    request = parse_request(raw_bytes)
-
-    # Handle request
-    resp = route_request(request)
-
-    conn.sendall(resp)
-    conn.close()
+    with server_socket:
+        while True:
+            conn, _ = server_socket.accept()
+            thread = threading.Thread(target=handle_conn, args=(conn), daemon=True)
+            thread.start()
 
 if __name__ == "__main__":
     main()
