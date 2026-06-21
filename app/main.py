1d9e37d6fed741415ad898a521f0bcb766226cc8
Brinley Macnamara <brinleymacnamara@Brinleys-MacBook-Pro.local>
2026-06-21 03:40:23 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def main():
     if target.startswith("/echo"):
         echo = target.split("/")[-1].encode()
         length = len(echo).to_bytes()
-        resp += b"Content-Type: text/plain\r\n" + b"Content-Length:" + length + b"\r\n\r\n" + echo
+        resp = b"HTTP/1.1 200 OK\r\n" + b"Content-Type: text/plain\r\n" + b"Content-Length:" + length + b"\r\n\r\n" + echo
     elif target != "/":
         resp = NOT_FOUND
 
