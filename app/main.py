3957c6ab0b692d53e59bef4303318afaed651c10
Brinley Macnamara <brinleymacnamara@Brinleys-MacBook-Pro.local>
2026-06-21 01:34:28 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 import socket
+from urllib.parse import urlsplit
 
 
 def main():
-    OK_RESP = b"HTTP/1.1 200 OK\r\n\r\n"
+    OK = b"HTTP/1.1 200 OK\r\n\r\n"
+    NOT_FOUND = b"HTTP/1.1 404 Not Found\r\n\r\n"
+    
+    # Setup connection
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, _ = server_socket.accept() # wait for client
-    conn.sendall(OK_RESP)
-    conn.close()
 
+    # Receive + parse request
+    bytes = conn.recv()
+    data = bytes.decode()
+    parts = data.split(" ")
+    target_url = parts[1]
+    path = urlsplit(target_url).path
+
+    # Handle request
+    resp = OK
+    if len(path) > 0: #
+        resp = NOT_FOUND
+
+    conn.sendall(resp)
+    conn.close()
 
 if __name__ == "__main__":
     main()
