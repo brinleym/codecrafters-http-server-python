3a130d9e1ce87e4c73fe6f9523d40f4f11c48d93
Brinley Macnamara <brinleymacnamara@Brinleys-MacBook-Pro.local>
2026-06-21 01:12:26 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ import socket
 
 
 def main():
+    OK_RESP = b"HTTP/1.1 200 OK\r\n\r\n"
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
+    server_socket.send(OK_RESP)
 
 
 if __name__ == "__main__":
