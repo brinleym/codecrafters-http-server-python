859c000f51cb1c0e2eb4489aab3dca14c867e95c
Brinley Macnamara <brinleymacnamara@Brinleys-MacBook-Pro.local>
2026-06-21 21:33:38 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -16,6 +16,7 @@ class HttpRequest:
     target: str
     version: str
     headers: dict[str, str]
+    body: str
 
 @dataclass
 class HttpResponse:
@@ -63,32 +64,42 @@ class HttpServer:
         return encoded + self.CRLF + body
         
     def handle_request(self, request: HttpRequest) -> HttpResponse:
+        method = request.method
         target = request.target
         headers = request.headers
 
-        if target == "/":
-            return HttpResponse(HTTPStatusCode.OK)
-        elif target.startswith("/echo"):
-            echo_string = target.split("/")[-1]
-            return HttpResponse(HTTPStatusCode.OK, {"Content-Type": "text/plain"}, echo_string)
-        elif target.startswith("/files"):
-            filename = target.split("/")[-1]
-            file_path = Path(f"/{self.root}/{filename}")
-            if file_path.exists():
-                with open(file_path, "r") as file:
-                    content = file.read()
-                    return HttpResponse(HTTPStatusCode.OK, {"Content-Type": "application/octet-stream"}, content)
+        if method == "POST":
+            if target.startswith("/files"):
+                filename = target.split("/")[-1]
+                file_path = Path(f"/{self.root}/{filename}")
+                with open(file_path, "w") as file:
+                    file.write(request.body)
             else:
                 return HttpResponse(HTTPStatusCode.NOT_FOUND)
-        elif target == "/user-agent":
-            user_agent_string = headers["user-agent"]
-            return HttpResponse(HTTPStatusCode.OK, {"Content-Type": "text/plain"}, user_agent_string)
         else:
-            return HttpResponse(HTTPStatusCode.NOT_FOUND)
+            if target == "/":
+                return HttpResponse(HTTPStatusCode.OK)
+            elif target.startswith("/echo"):
+                echo_string = target.split("/")[-1]
+                return HttpResponse(HTTPStatusCode.OK, {"Content-Type": "text/plain"}, echo_string)
+            elif target.startswith("/files"):
+                filename = target.split("/")[-1]
+                file_path = Path(f"/{self.root}/{filename}")
+                if file_path.exists():
+                    with open(file_path, "r") as file:
+                        content = file.read()
+                        return HttpResponse(HTTPStatusCode.OK, {"Content-Type": "application/octet-stream"}, content)
+                else:
+                    return HttpResponse(HTTPStatusCode.NOT_FOUND)
+            elif target == "/user-agent":
+                user_agent_string = headers["user-agent"]
+                return HttpResponse(HTTPStatusCode.OK, {"Content-Type": "text/plain"}, user_agent_string)
+            else:
+                return HttpResponse(HTTPStatusCode.NOT_FOUND)
 
-    def parse_request(self, raw_bytes: bytes) -> HttpRequest:
-        request = raw_bytes.decode()
-        lines = request.splitlines()
+    def parse_request(self, headers_part: bytes, body_part: bytes) -> HttpRequest:
+        headers_text = headers_part.decode()
+        lines = headers_text.splitlines()
 
         # request line
         request_line = lines[0]
@@ -103,20 +114,42 @@ class HttpServer:
             header_type, value = line.split(":", maxsplit=1)
             headers[header_type.strip().lower()] = value.strip()
 
-        return HttpRequest(method, target, version, headers)
+        return HttpRequest(method, target, version, headers, body_part.decode())
         
     def handle_conn(self, conn: socket):
         with conn:
             while True: 
-                raw_bytes = conn.recv(1024) # Receive data from socket
+                raw_bytes = b""
                 
-                if not raw_bytes: # Detect closed connection
-                    break
+                while b"\r\n\r\n" not in raw_bytes:
+                    chunk = conn.recv(4096)
+                    if not chunk:
+                        break
+                    
+                    raw_bytes += chunk
+
+                headers_part, body_part = raw_bytes.split(b"\r\n\r\n", maxsplit=1)
+                headers_text = headers_part.decode()
+
+                content_length = 0
+                for line in headers_text.split("\r\n"):
+                    if line.lower().startswith("content-length:"):
+                        content_length = int(line.split(":")[1].strip())
+                        break
+
+                while len(body_part) < content_length:
+                    remaining_bytes = content_length - len(body_part)
+                    
+                    chunk = conn.recv(min(4096, remaining_bytes))
+                    if not chunk:
+                        break
+                   
+                    body_part += chunk
             
                 request = self.parse_request(raw_bytes)
-                response = self.format_response(self.handle_request(request))
-
-                conn.sendall(response)
+                response = self.handle_request(request)
+                response_bytes = self.format_response(response)
+                conn.sendall(response_bytes)
 
     def start(self):
         with self.sock:
